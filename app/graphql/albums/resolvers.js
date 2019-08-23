aeeca1025904c11d3756294420fffe5b87eae5ae
YesicaNavaB <yesica.betancourt@wolox.co>
2019-08-23 13:37:49 -0500

    corrections

--- app/graphql/albums/resolvers.js
+++ app/graphql/albums/resolvers.js
@@ -2,19 +2,14 @@ const config = require('../../../config'),
   { url } = config.common.apiAlbums,
   { getAlbumSources } = require('../../services/albums');
 
-const getAlbum = (_, params) =>
-  getAlbumSources(`${url}albums/${params.id}`).then(album => ({
-    id: album.id,
-    title: album.title,
-    artist: album.userId
-  }));
+const getAlbum = (_, params) => getAlbumSources(`${url}albums/${params.id}`);
 
 module.exports = {
   Query: {
     album: getAlbum
   },
   Album: {
-    id: root => root.id,
-    photos: params => getAlbumSources(`${url}photos?albumId=${params.id}`)
+    artist: root => root.artist,
+    photos: root => getAlbumSources(`${url}photos?albumId=${root.id}`)
   }
 };
