792e348198b007aba73a276028dceca02d9130e0
YesicaNavaB <yesica.betancourt@wolox.co>
2019-08-27 11:33:58 -0500

    make generic the filter of the albums

--- app/graphql/albums/resolvers.js
+++ app/graphql/albums/resolvers.js
@@ -1,13 +1,14 @@
 const config = require('../../../config'),
   { url } = config.common.apiAlbums,
   { getAlbumAndPhotos } = require('../../services/albums'),
-  { orderArrayByField, filterArrayByTitle } = require('../../helpers/arraysFields');
+  { orderArrayByField, filterArrayByField } = require('../../helpers/arraysFields'),
+  { filterTitleAlbum } = require('../../constants/albumsConstants');
 
 const getAlbum = (_, params) => getAlbumAndPhotos(`${url}albums/${params.id}`);
 const getAlbumsList = (_, params) =>
   getAlbumAndPhotos(`${url}albums`).then(result => {
     const startPage = params.offset * params.limit;
-    const albumsFilter = filterArrayByTitle(result, params.filter);
+    const albumsFilter = filterArrayByField(result, params.filter, filterTitleAlbum);
     const albumsPagitation = albumsFilter.slice(startPage, startPage + params.limit);
     return orderArrayByField(albumsPagitation, params.orderBy);
   });
--- app/helpers/arraysFields.js
+++ app/helpers/arraysFields.js
@@ -1,4 +1,4 @@
 exports.orderArrayByField = (array, orderBy) => array.sort((a, b) => (a[orderBy] >= b[orderBy] ? 1 : -1));
 
-exports.filterArrayByTitle = (array, filter) =>
-  array.filter(obj => obj.title.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+exports.filterArrayByField = (array, query, field) =>
+  array.filter(obj => obj[field].toLowerCase().indexOf(query.toLowerCase()) !== -1);
