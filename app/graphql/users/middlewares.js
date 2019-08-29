2f621da166d0f69277d393e73684877b3113f7a8
YesicaNavaB <yesica.betancourt@wolox.co>
2019-08-29 08:19:40 -0500

    sign up validations, using yup validators

--- app/graphql/users/middlewares.js
+++ app/graphql/users/middlewares.js
@@ -1,8 +1,44 @@
-const logger = require('../../logger');
+const logger = require('../../logger'),
+  yup = require('yup'),
+  { user: User } = require('../../models'),
+  errors = require('../../errors');
 
-const createUser = (resolve, root, args) => {
-  logger.info("Middleware for 'createUser' mutation");
-  // Add different actions that you want to be executed before your resolver, i.e: input validation or caching
+const regexEmail = new RegExp(
+  /^(([^<>\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@wolox.(co|com|com.ar)\s*$/
+);
+const regexAlphanumeric = new RegExp(/^[a-zA-Z0-9_]*$/);
+
+const schema = yup.object().shape({
+  password: yup
+    .string()
+    .required('password is required')
+    .min(8, 'Must be at least 8 chars long')
+    .matches(regexAlphanumeric, 'email is not valid or does not belong to the wolox domain'),
+  email: yup
+    .string()
+    .required('email is required')
+    .email('email is not valid')
+    .matches(regexEmail, 'email is not valid or does not belong to the wolox domain')
+});
+
+const createUser = async (resolve, root, args) => {
+  try {
+    await schema.validate(args.user, { abortEarly: false });
+  } catch (err) {
+    logger.err(err.message);
+    throw errors.signUpError(err.message);
+  }
+  const { email } = args.user;
+
+  const userAlreadyExists = await User.findAndCountAll({
+    where: { email },
+    select: ['id']
+  });
+
+  if (userAlreadyExists.count === 1) {
+    throw errors.dataBaseError('User already exists');
+  }
+
   return resolve(root, args);
 };
 
