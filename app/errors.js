738808fceed402288f6b9c8dda3efb030272f6b7
YesicaNavaB <52759744+YesicaNavaB@users.noreply.github.com>
2019-08-30 11:15:14 -0500

    signUp (#6)
    signUp completed!

--- app/errors.js
+++ app/errors.js
@@ -4,8 +4,10 @@ const createError = (message, statusCode) => new ApolloError(message, statusCode
 
 const DEFAULT_ERROR = 500,
   SERVICE_UNAVAILABLE = 503,
-  BAD_REQUEST = 400;
+  BAD_REQUEST = 400,
+  DATABASE_ERROR = 503;
 
 exports.defaultError = message => createError(message, DEFAULT_ERROR);
 exports.badRequest = message => createError(message, BAD_REQUEST);
 exports.serviceUnavailable = message => createError(message, SERVICE_UNAVAILABLE);
+exports.dataBaseError = message => createError(message, DATABASE_ERROR);
--- app/graphql/users/resolvers.js
+++ app/graphql/users/resolvers.js
@@ -1,10 +1,23 @@
 const { user: User } = require('../../models'),
-  { userLoggedIn } = require('../events');
+  { userLoggedIn } = require('../events'),
+  { encryptionString } = require('../../helpers/encryption'),
+  logger = require('../../logger'),
+  errors = require('../../errors');
 
 const getUser = (_, params) => User.getOne(params);
 const getUsers = (_, params) => User.getAll(params);
 
-const createUser = (_, { user }) => User.createModel(user);
+const createUser = (_, { user }) =>
+  User.createModel({ ...user, password: encryptionString(user.password) })
+    .then(result => {
+      logger.info(`the user was created correctly: ${user.firstName}`);
+      return result;
+    })
+    .catch(err => {
+      logger.error(`Could not create user: ${user.firstName}`);
+      throw errors.dataBaseError(err.message);
+    });
+
 const logIn = (_, { credentials }) => {
   // IMPORTANT: Not a functional login, its just for illustrative purposes
   userLoggedIn.publish(credentials.firstName);
